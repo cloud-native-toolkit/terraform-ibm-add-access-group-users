24cb6edbff951a3b7ea497fd4bf6b7ba96913d41
Sean Sundberg <seansund@us.ibm.com>
2021-02-11 15:03:19 -0600

    Log in to ibmcloud first in validate deploy. duh

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+ibmcloud login --apikey "${IBMCLOUD_API_KEY}" -r us-east
+
 RESOURCE_GROUP=$(cat ./terraform.tfvars | grep "new_resource_group" | sed -E "s/.*=//g" | sed 's/"//g')
 
 echo "Resource group: ${RESOURCE_GROUP}"
