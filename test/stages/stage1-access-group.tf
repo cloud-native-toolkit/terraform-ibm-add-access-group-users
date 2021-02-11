38767497affe6b63ed5e3f6d7e1ff090bd2e038a
Sean Sundberg <seansund@us.ibm.com>
2021-02-11 14:44:18 -0600

    Updates test variables

--- test/stages/stage1-access-group.tf
+++ test/stages/stage1-access-group.tf
@@ -2,6 +2,6 @@ module "access_groups" {
   source = "github.com/ibm-garage-cloud/terraform-ibm-access-group.git"
 
   region               = var.region
-  resourceGroupNames   = var.new_resource_group
+  resourceGroupNames   = [var.new_resource_group]
   createResourceGroups = true
 }
