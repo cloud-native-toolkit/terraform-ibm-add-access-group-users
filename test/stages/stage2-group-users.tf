3f9c1c28aac6e6222deaad59e76ba6282338f684
Vamsy Sivaarcca <vamsysivaarcca@vamsys-mbp.lan>
2022-06-30 11:11:28 -0400

    Updated stage2-group-users.tf
    Closes #2
    
    Signed-off-by: Vamsy Sivaarcca <vamsysivaarcca@vamsys-mbp.lan>

--- test/stages/stage2-group-users.tf
+++ test/stages/stage2-group-users.tf
@@ -2,19 +2,19 @@ module "admin-group" {
   source = "./module"
 
   users = module.admin-users.users
-  access_groups = module.access_groups.adminGroupNames
+  access_groups = module.access_groups
 }
 
 module "edit-group" {
   source = "./module"
 
   users = module.edit-users.users
-  access_groups = module.access_groups.editGroupNames
+  access_groups = module.access_groups
 }
 
 module "view-group" {
   source = "./module"
 
   users = module.view-users.users
-  access_groups = module.access_groups.viewGroupNames
+  access_groups = module.access_groups
 }
