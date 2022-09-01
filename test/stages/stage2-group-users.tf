d2c05c40ea8a82e5b44054d85828ca5af5de6eb3
balasgit <bala@us.ibm.com>
2022-09-01 17:18:18 -0500

    Fix compile errors

--- test/stages/stage2-group-users.tf
+++ test/stages/stage2-group-users.tf
@@ -2,19 +2,19 @@ module "admin-group" {
   source = "./module"
 
   users = module.admin-users.users
-  access_groups = module.access_groups.adminGroupNames
+  #6 access_groups = module.access_groups.adminGroupNames
 }
 
 module "edit-group" {
   source = "./module"
 
   users = module.edit-users.users
-  access_groups = module.access_groups.editGroupNames
+  #6 access_groups = module.access_groups.editGroupNames
 }
 
 module "view-group" {
   source = "./module"
 
   users = module.view-users.users
-  access_groups = module.access_groups.viewGroupNames
+  #6 access_groups = module.access_groups.viewGroupNames
 }
