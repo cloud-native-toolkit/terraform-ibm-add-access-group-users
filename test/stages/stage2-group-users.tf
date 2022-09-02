af22993621c1443d18e60835fee854ccb24d184a
balasgit <bala@us.ibm.com>
2022-09-02 10:42:30 -0500

    updated the test stages

--- test/stages/stage2-group-users.tf
+++ test/stages/stage2-group-users.tf
@@ -2,19 +2,27 @@ module "admin-group" {
   source = "./module"
 
   users = module.admin-users.users
-  access_groups = module.access_groups.view_group_name
+  depends_on = [
+    module.access_groups
+  ]
 }
+
+#https://github.com/terraform-ibm-modules/terraform-ibm-toolkit-access-group/blob/main/outputs.tf
 
 module "edit-group" {
   source = "./module"
 
   users = module.edit-users.users
-  access_groups = module.access_groups.view_group_name
+  depends_on = [
+    module.access_groups
+  ]
 }
 
 module "view-group" {
   source = "./module"
 
   users = module.view-users.users
-  access_groups = module.access_groups.view_group_name
+  depends_on = [
+    module.access_groups
+  ]
 }
