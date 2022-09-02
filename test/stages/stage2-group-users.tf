6157366d88ca072385fd20f22386af94a5666b2d
balasgit <bala@us.ibm.com>
2022-09-02 10:35:43 -0500

    update to module ref variable

--- test/stages/stage2-group-users.tf
+++ test/stages/stage2-group-users.tf
@@ -2,15 +2,14 @@ module "admin-group" {
   source = "./module"
 
   users = module.admin-users.users
-  access_groups = module.access_groups.admin_group_name
-  
+  access_groups = module.access_groups.view_group_name
 }
 
 module "edit-group" {
   source = "./module"
 
   users = module.edit-users.users
-  access_groups = module.access_groups.edit_group_name
+  access_groups = module.access_groups.view_group_name
 }
 
 module "view-group" {
