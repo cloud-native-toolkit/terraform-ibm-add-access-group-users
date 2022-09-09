4188b39d7a52f2ae2c07ae46333b81dea39a675c
balasgit <bala@us.ibm.com>
2022-09-09 10:39:33 -0500

    Fix compile error

--- test/stages/stage1-access-group.tf
+++ test/stages/stage1-access-group.tf
@@ -2,7 +2,7 @@ module "access_groups" {
   source = "github.com/ibm-garage-cloud/terraform-ibm-access-group.git"
 
   ibmcloud_api_key    = var.ibmcloud_api_key
-  #resource_group_name = var.resource_group_name
-  resource_group_name = module.access_groups.resource_group_name
+  resource_group_name = var.resource_group_name
+  
 
 }
