ae35f022b10cfd781f88f315d2fefcda57bbe43e
balasgit <bala@us.ibm.com>
2022-09-15 10:08:47 -0500

    Added Resource Group

--- test/stages/stage1-access-group.tf
+++ test/stages/stage1-access-group.tf
@@ -2,7 +2,6 @@ module "access_groups" {
   source = "github.com/ibm-garage-cloud/terraform-ibm-access-group.git"
 
   ibmcloud_api_key    = var.ibmcloud_api_key
-  resource_group_name = var.resource_group_name
-  
+  resource_group_name = module.resource_group.name
 
 }
