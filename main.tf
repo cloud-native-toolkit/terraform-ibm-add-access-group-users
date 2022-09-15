a2c816e2c84708118465ea6a8b1c6414732de43e
balasgit <bala@us.ibm.com>
2022-09-15 09:43:08 -0500

    Printing users / AG

--- main.tf
+++ main.tf
@@ -10,3 +10,9 @@ resource ibm_iam_access_group_members group_members {
   access_group_id = element(data.ibm_iam_access_group.access_groups.*.groups.0.id, count.index)
   ibm_ids         = var.users
 }
+
+resource null_resource print_users {
+  provisioner "local-exec" {
+    command = "echo 'List of users : ${var.users}' 'AG: ${access_group_name}'"
+  }
+}
