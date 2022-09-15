94aa048fe4efc556dc518da339fd2d43f61f0992
balasgit <bala@us.ibm.com>
2022-09-15 10:14:00 -0500

    Fix compile errors
    Signed-off-by: balasgit <bala@us.ibm.com>

--- main.tf
+++ main.tf
@@ -10,9 +10,3 @@ resource ibm_iam_access_group_members group_members {
   access_group_id = element(data.ibm_iam_access_group.access_groups.*.groups.0.id, count.index)
   ibm_ids         = var.users
 }
-
-resource null_resource print_users {
-  provisioner "local-exec" {
-    command = "echo 'List of users : ${var.count}'"
-  }
-}
